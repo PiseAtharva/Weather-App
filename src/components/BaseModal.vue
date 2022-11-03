a9035694011be3a393d2fc80665c1efb829deda5
Pise Atharva (IFIN IT RDI RD) <Atharva.Pise@infineon.com>
2022-11-03 15:34:00 +0530

    added transition over complete BaseModal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,18 +1,25 @@
 <template>
-  <div
-    v-show="modalActive"
-    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
-  >
-    <div
-      v-if="modalActive"
-      class="p-4 bg-white self-start mt-32 max-w-screen-md"
-    >
-      <slot />
-      <button class="text-white mt-8 bg-weather-primary py-2 px-6" @click="$emit('close-modal')">
-        Close
-      </button>
-    </div>
-  </div>
+    <Transition name="modal-outer">
+      <div
+        v-show="modalActive"
+        class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+      >
+        <Transition name="modal-inner">
+          <div
+            v-if="modalActive"
+            class="p-4 bg-white self-start mt-32 max-w-screen-md"
+          >
+            <slot />
+            <button
+              class="text-white mt-8 bg-weather-primary py-2 px-6"
+              @click="$emit('close-modal')"
+            >
+              Close
+            </button>
+          </div>
+        </Transition>
+      </div>
+    </Transition>
 </template>
 
 <script setup>
@@ -25,4 +32,35 @@ defineProps({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+/* Transition Animation duration for entering and leaving  */
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+/* Transition Property to be used */
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+
+/* Transition Animation Duration for content of Modal */
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+/* Transition Animation for Modal Content */
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
